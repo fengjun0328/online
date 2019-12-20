63fbd90ecaec8e5bea8fe679287f81eba195bae7
admin <783585655@qq.com>
2019-12-20 16:19:33 +0800

    数据

---
+++ addressonline/src/main/java/cn/bdqn/pojo/Contact.java
@@ -0,0 +1,48 @@
+package cn.bdqn.pojo;
+
+public class Contact {
+	private Long id;
+	private String cname;
+	private String mobile;
+	private String qq;
+	private String email;
+	private String address;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getQq() {
+		return qq;
+	}
+	public void setQq(String qq) {
+		this.qq = qq;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+
+}
